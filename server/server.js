8e8dc4d5302cfe20b4720f6fa5e1cc437005182c
jeodom17 <jeodom17@gmail.com>
2021-11-19 17:56:33 -0500

    add initial file structure

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require("path");
+const { ApolloServer } = require("apollo-server-express");
+const db = require("./config/connection");
+// const routes = require("./routes");
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+
+
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`Now listening on localhost:${PORT}`);
+    console.log(
+      `Use GraphQL at http://localhost:${PORT}${ApolloServer.graphqlPath}`
+    );
+  });
+});
