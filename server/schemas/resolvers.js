92513c06e61e0a6ec56952a57c6d98dc73ee1d0e
Leci1259 <lreid31@gatech.edu>
2021-11-23 11:31:27 -0500

    completed resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,70 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Topic } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        },
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+        addComment: async (parent, { commentData }, context) => {
+            if (context.topic) {
+                const updatedTopic = await Topic.findByIdAndUpdate(
+                    { _id: context.topic._id },
+                    { $push: { comments: bookData } },
+                    { new: true }
+                );
+
+                return updatedUser;
+            }
+
+            throw new AuthenticationError('You need to be logged in!');
+        },
+        removeComment: async (parent, { commentId }, context) => {
+            if (context.topic) {
+                const updatedTopic = await Topic.findOneAndUpdate(
+                    { _id: context.topic._id },
+                    { $pull: { comments: { commentId } } },
+                    { new: true }
+                );
+
+                return updatedTopic;
+            }
+
+            throw new AuthenticationError('You need to be logged in!');
+        },
+    },
+};
+
+module.exports = resolvers;
