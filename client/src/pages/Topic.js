4a85979e7afd5e139b0729b043b2028861d98d24
jeodom17 <jeodom17@gmail.com>
2021-11-24 13:29:33 -0500

    link topic page

--- client/src/pages/Topic.js
+++ client/src/pages/Topic.js
@@ -1,12 +1,11 @@
 import React from "react";
-import Land from "../components/Land";
 
 export default function Topic() {
     return (
       <div>
         
+        <h1>This is the page for the SELECTED Topic </h1>
         
-        <Land/>
 
       
       </div>
