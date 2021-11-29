d0a51e4ccdc8371534275dbb9b52570ee0eb97bd
Abraham Hong <hhmfjg@gmail.com>
2021-11-29 11:17:08 -0500

    static image, button route

--- client/src/components/Land/index.js
+++ client/src/components/Land/index.js
@@ -1,16 +1,16 @@
 import React, { useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
-import jungaroo from "../../assets/jungaroo.png";
+import circletext from "../../assets/circletext.png";
 import "./style.css";
 
 export default function Land() {
   return (
     <div className="landing">
       <div className="landing-header">
-        <img src={jungaroo} className="landing-logo" alt="logo" />
+        <img src={circletext} className="landing-logo" alt="logo" />
       </div>
       <Link
-        to="/"
+        to="/account"
         className="center waves-effect waves-light btn-large findbtn"
       >
         LOGIN / SIGNUP
