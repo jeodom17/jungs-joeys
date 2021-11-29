6c72858012f5d9f743a5198ce28b3b5c06db64e2
jeodom17 <jeodom17@gmail.com>
2021-11-29 11:58:29 -0500

    set up modal files

--- client/src/pages/Topic.js
+++ client/src/pages/Topic.js
@@ -1,4 +1,6 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+
 import { useSearchParams } from "react-router-dom";
 import { useMutation, useQuery } from "@apollo/client";
 import { GET_ME, GET_POSTS } from "../utils/queries";
@@ -23,31 +25,40 @@ export default function Topic() {
   //*console.log(searchParams.get("name"))
   //* })
   return (
-    <div>
-      <h3>
-        Welcome to the HTML study forum!
-      </h3>
-      <button class="waves-effect waves-light btn-large">Ask a Question</button>
+    <>
+      <div>
+        <h3>Welcome to the HTML study forum!</h3>
+        <button class="waves-effect waves-light btn-large">
+          Ask a Question
+        </button>
+      </div>
+
       <div className="container">
-      <div class="row">
-        <div class="col s12 m12">
-          <div class="card blue-grey darken-1">
-            <div class="card-content white-text">
-              <span class="card-title">Card Title</span>
-              <p>
-                Question Example - I am a very simple card. I am good at containing small bits of
-                information. I am convenient because I require little markup to
-                use effectively.
-              </p>
-            </div>
-            <div class="card-action">
-              <a href="#">See Comments</a>
-              <a href="#">Comment Button</a>
+        <div class="row">
+          <div class="col s12 m12">
+            <div class="blue-grey darken-1">
+              <div class="card-content white-text">
+                <h5>User Name</h5>
+                <p>Date HERE</p>
+                <span class="card-title">Post Title</span>
+                <p>
+                  Question Example - I am a very simple card. I am good at
+                  containing small bits of information. I am convenient because
+                  I require little markup to use effectively.
+                </p>
+              </div>
+              <div class="card-action">
+                <span class="comments material-icons">forum</span>
+                <Link to="#">See Comments</Link>
+                <a class="waves-effect waves-light btn">
+                  Add Comment
+                  <i class="add-comment material-icons">insert_comment</i>
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    </div>
+    </>
   );
 }
