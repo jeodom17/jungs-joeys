d1ff2076a9342ad542586bd78416b9324bf40fd1
jeodom17 <jeodom17@gmail.com>
2021-11-29 14:17:26 -0500

    change text in  3 modals

--- client/src/pages/Topic.js
+++ client/src/pages/Topic.js
@@ -8,36 +8,36 @@ import { useMutation, useQuery } from "@apollo/client";
 import { GET_ME, GET_TOPIC_BY_NAME } from "../utils/queries";
 
 export default function Topic() {
-  const [searchParams, setSearchParams] = useSearchParams();
+  // const [searchParams, setSearchParams] = useSearchParams();
   
   const { loading, data } = useQuery(GET_TOPIC_BY_NAME, {
-    variables: { name: searchParams.get("name")}
+  //   variables: { name: searchParams.get("name")}
   });
 
-  // if data isn't here yet, say so
-  if (loading) {
-    return <h2>LOADING.......</h2>;
-  }
+  // // if data isn't here yet, say so
+  // if (loading) {
+  //   return <h2>LOADING.......</h2>;
+  // }
   
-  console.log(data)
-  const postData = data?.getTopicByName || [];
-  console.log(postData)
-
+  // console.log(data)
+   const postData = data?.getTopicByName || [];
   // console.log(postData)
+
+  // // console.log(postData)
   
-  //! useEffect(() => {
-    //! console.log("HERE");
+  // //! useEffect(() => {
+  //   //! console.log("HERE");
 
-    //* use searchParams.get("name") in a graphQL call to the Topic db to populate page with the correct questions
-    //* need to use useQuery with the correct queries and typedefs to get the topics to populate here
-    // console.log(searchParams.get("name"))
-  //! })
+  //   //* use searchParams.get("name") in a graphQL call to the Topic db to populate page with the correct questions
+  //   //* need to use useQuery with the correct queries and typedefs to get the topics to populate here
+  //   // console.log(searchParams.get("name"))
+  // //! })
     return (
-      <div>
-        
+      <>
+      <div>  
         <h1>{postData}</h1>
-        
-
+      </div> 
+     
       <div className="container">
         <div class="row">
           <div class="col s12 m12">
@@ -56,8 +56,6 @@ export default function Topic() {
                 <span class="comments material-icons">forum</span>
                 <SeeCommModal />
                 <CommModal />
-                  
-               
               </div>
             </div>
           </div>
