637f6a83dfef6139d72d708db6cbd8e35277d072
jeodom17 <jeodom17@gmail.com>
2021-11-29 23:57:49 -0500

    set up form on addcomment and ask question modals

--- client/src/components/CommModal/index.js
+++ client/src/components/CommModal/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import M from "materialize-css";
-import "materialize-css/dist/css/materialize.min.css";   
+import "materialize-css/dist/css/materialize.min.css";
 
 class CommModal extends Component {
   componentDidMount() {
@@ -22,7 +22,7 @@ class CommModal extends Component {
       opacity: 0.5,
       dismissible: false,
       startingTop: "4%",
-      endingTop: "10%"
+      endingTop: "10%",
     };
     M.Modal.init(this.Modal, options);
 
@@ -39,13 +39,12 @@ class CommModal extends Component {
           className="qModBtn waves-effect waves-light btn modal-trigger"
           data-target="modal3"
         >
-          Add a Comment
-          &nbsp;&nbsp;&nbsp;
+          Add a Comment &nbsp;&nbsp;&nbsp;
           <i class="add-comment material-icons">insert_comment</i>
         </a>
 
         <div
-          ref={Modal => {
+          ref={(Modal) => {
             this.Modal = Modal;
           }}
           id="modal3"
@@ -56,16 +55,40 @@ class CommModal extends Component {
                         If you want Fixed Footer Modal then add
                         modal-fixed-footer to the "modal" div*/}
           <div className="modal-content">
-            <h4>Add Your Comment Below</h4>
-            <p>Add Comment Form Goes Here</p>
+            <h5>Add Your Comment Below</h5>
           </div>
+
+          <div class="divider"></div>
+          
+          <div className="container"></div>
+          <div class="row">
+            <form class="col s12">
+              <div class="row">
+                <div class="input-field col s10 push-s1">
+                  <textarea
+                    id="textarea1"
+                    class="materialize-textarea"
+                  ></textarea>
+                  <label for="textarea1">Enter your comments here</label>
+                </div>
+                <button
+                  class="btn waves-effect waves-light col s6 push-s3"
+                  type="submit"
+                  name="action"
+                >
+                  Post Comments
+                  <i class="material-icons right">send</i>
+                </button>
+              </div>
+            </form>
+          </div>
+          </div>
+          <div>
           <div className="modal-footer">
             <a className="modal-close waves-effect waves-red btn-flat">
               Disagree
             </a>
-            <a className="modal-close waves-effect waves-green btn">
-              Close
-            </a>
+            <a className="modal-close waves-effect waves-green btn">Close</a>
           </div>
         </div>
       </div>
--- client/src/pages/Topic.js
+++ client/src/pages/Topic.js
@@ -1,11 +1,11 @@
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
+//* import { Link } from "react-router-dom";
 import SeeCommModal from "../components/SeeCommModal";
 import CommModal from "../components/CommModal";
 import QuesModal from "../components/QuesModal";
 import { useSearchParams } from "react-router-dom";
-import { useMutation, useQuery } from "@apollo/client";
-import { GET_ME, GET_TOPIC_BY_NAME } from "../utils/queries";
+import { useQuery } from "@apollo/client";
+import { GET_TOPIC_BY_NAME } from "../utils/queries";
 
 export default function Topic() {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -25,13 +25,7 @@ export default function Topic() {
   const postData = data?.getTopicByName || [];
   console.log(postData)
 
-  //! useEffect(() => {
-    //! console.log("HERE");
-
-    //* use searchParams.get("name") in a graphQL call to the Topic db to populate page with the correct questions
-    //* need to use useQuery with the correct queries and typedefs to get the topics to populate here
-    // console.log(searchParams.get("name"))
-  //! })
+  
     return (
       <>
       <div className="row">
@@ -39,8 +33,10 @@ export default function Topic() {
         <h3>Welcome{postData}</h3>
       </div> 
 
-      <div className="col s4 offset-s4 qmodalBtn">
+      <div className="col s4 offset-s4 section qmodalBtn">
+
       <QuesModal />
+
       </div>
       </div>
 
@@ -48,8 +44,9 @@ export default function Topic() {
         <div class="row">
           <div class="col s12 m12">
             <div class="blue-grey darken-1">
-              <div class="card-content white-text">
-                <h5 className="col s3 offset-s1">User Name</h5>
+              <div class="card-content white-text">          
+              <i class="material-icons prefix col s1">account_circle</i>
+                <h5 className="col s3">User Name</h5>
                 <p className="col s2 offset-s6">Date HERE</p>
                 <span class="card-title col s12">Post Title</span>
                 <p className="col s12">
@@ -58,13 +55,19 @@ export default function Topic() {
                   I require little markup to use effectively.
                 </p>
               </div>
+              
               <div class="card-action">
                 <span class="comments material-icons col s1">forum</span>
                 <div className="col s4">
+
                 <SeeCommModal />
+
                 </div>
+
                 <div className="commModalBtn">
+
                 <CommModal />
+
                 </div>
               </div>
             </div>
