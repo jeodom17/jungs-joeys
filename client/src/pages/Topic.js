9ca52794d2eaa929d75aab5e03cf96f0545bbfe3
Abraham Hong <hhmfjg@gmail.com>
2021-11-30 16:06:43 -0500

    forum/topic page

--- client/src/pages/Topic.js
+++ client/src/pages/Topic.js
@@ -6,13 +6,14 @@ import QuesModal from "../components/QuesModal";
 import { useSearchParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { GET_TOPIC_BY_NAME } from "../utils/queries";
+import "./styles/topic.css";
 
 const Topic = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const forumName = searchParams.get("name");
 
   const { loading, error, data } = useQuery(GET_TOPIC_BY_NAME, {
-    variables: { name: searchParams.get("name") }
+    variables: { name: searchParams.get("name") },
   });
 
   // if data isn't here yet, say so
@@ -22,46 +23,42 @@ const Topic = () => {
 
   const postData = data?.getTopicByName || [];
   // console.log(postData.posts[0].content)
-  const postArray = postData.posts
+  const postArray = postData.posts;
 
   for (let i = 0; i < postArray.length; i++) {
-
     const postContent = postData.posts[i].content;
     const postAuthor = postData.posts[i].author.username;
-    console.log(postAuthor)
+    console.log(postAuthor);
 
     return (
       <>
-        <div className="container">
-          <div className="row">
+        <div className="container forum-topic">
+          <div className="row forum-content">
             <div className="col s8">
-              <h4>
-                Welcome to the {forumName} Forum
-              </h4>
+              <h4 className="topic-header">Welcome to the {forumName} Forum</h4>
             </div>
             <div className="col s4">
               <QuesModal />
             </div>
             <div className="col s12 m12">
-              <div className="blue-grey darken-1">
-                <div className="card-content white-text">
-                  <h5>By: {postAuthor}</h5>
+              <div className="darken-1 topic-card">
+                <div className="card-content topic-text">
+                  <h5 className="post-author">By: {postAuthor}</h5>
                   {/* <span class="card-title">Post Title</span> */}
-                  <p>
-                    {postContent}
-                  </p>
+                  <p>{postContent}</p>
                 </div>
                 {/* display flex on following div so that the button goes beside the input */}
-                <div className="commentForm">
-                  <div className="input-field commInput">
-                    <textarea
-                      id="textarea1"
-                      className="materialize-textarea"
-                    ></textarea>
+                <div className="comment-form">
+                  <div className="input-field comment-input col s8">
+                    <input
+                      id="comment_text"
+                      type="text"
+                      className="validate"
+                    ></input>
                     <label for="textarea1">Enter your comments here</label>
                   </div>
                   <button
-                    className="btn waves-effect waves-light col s6 postBtn"
+                    className="btn waves-effect waves-light col s2 postBtn"
                     type="submit"
                     name="action"
                   >
@@ -69,19 +66,20 @@ const Topic = () => {
                     <i className="material-icons right">send</i>
                   </button>
                 </div>
-                <div className="card-action">
-                  <span className="comments material-icons">forum</span>
-                  <h6>Comments</h6>
+                <div className="container comment-box card-action">
+                  <h6 className="comments-header">
+                    Comments{" "}
+                    <span className="comments material-icons">forum</span>
+                  </h6>
                   <SeeCommModal />
-                  <CommModal />
                 </div>
               </div>
             </div>
           </div>
         </div>
       </>
-    )
+    );
   }
-}
+};
 
 export default Topic;
