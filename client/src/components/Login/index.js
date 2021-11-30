33df9de7b1b3264ee13e9090c3434bbd751f944e
Leci1259 <lreid31@gatech.edu>
2021-11-30 11:46:08 -0500

    updated code for login/signup functionality

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,77 +1,91 @@
-import React, { Component } from "react";
+import React, { useState, } from "react";
+import { LOGIN_USER } from "../../utils/mutations";
+import { useMutation } from '@apollo/client';
+import Auth from '../../utils/auth';
 
-class Login extends Component {
-  constructor() {
-    super();
 
-    this.state = {
-      email: "",
-      password: ""
-    };
+const Login = () => {
+  const [userFormData, setUserFormData] = useState({ email: '', password: '' });
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  const [login, { error }] = useMutation(LOGIN_USER);
 
-  handleChange(event) {
-    let target = event.target;
-    let value = target.type === "checkbox" ? target.checked : target.value;
-    let name = target.name;
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
 
-    this.setState({
-      [name]: value
-    });
-  }
-
-  handleSubmit(event) {
+  const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    console.log("The form was submitted with the following data:");
-    console.log(this.state);
-  }
+    const form = event.currentTarget;
 
-  render() {
-    return (
-      <div className="formCenter">
-        <form className="formFields" onSubmit={this.handleSubmit}>
-          <div className="formField">
-            <label className="formFieldLabel" htmlFor="name">
-              USERNAME
-            </label>
-            <input
-              type="name"
-              id="name"
-              className="formFieldInput"
-              placeholder="Enter your USERNAME"
-              name="name"
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
-          </div>
+    event.preventDefault();
+    event.stopPropagation();
 
-          <div className="formField">
-            <label className="formFieldLabel" htmlFor="password">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="formFieldInput"
-              placeholder="Enter your PASSWORD"
-              name="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-            />
-          </div>
 
-          <div className="formField">
-            <button className="formFieldButton">Log In</button>{" "}
-          </div>
+    try {
+      const { data } = await login({
+        variables: { ...userFormData },
+      });
 
-        </form>
-      </div>
-    );
-  }
-}
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values
+    setUserFormData({
+      email: '',
+      password: '',
+    });
+  };
+
+  return (
+    <div>
+      <form className="formFields" onSubmit={handleFormSubmit}>
+        <div className="formField">
+          <label className="formFieldLabel" htmlFor="name">
+            Email
+          </label>
+          <input
+            type="name"
+            id="name"
+            className="formFieldInput"
+            placeholder="Your email"
+            name="email"
+            onChange={handleInputChange}
+            value={userFormData.email}
+          />
+        </div>
+
+        <div className="formField">
+          <label className="formFieldLabel" htmlFor="password">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="formFieldInput"
+            placeholder="Your password"
+            name="password"
+            onChange={handleInputChange}
+            value={userFormData.password}
+          />
+        </div>
+
+        <div className="formField">
+          <button
+            disabled={!(userFormData.email && userFormData.password)}
+            type="submit"
+            variant="success"
+            className="formFieldButton"
+          >
+            Log In
+          </button>
+        </div>
+      </form>
+    </div >
+  );
+};
 
 export default Login;
